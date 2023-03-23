21fa2861788e4f29de9152be830e6393c52df734
Venteri <velibor.venteri@gmail.com>
2023-03-23 22:19:19 +0100

    repaired the floor of maze

--- Project/Assets/Scripts/MazeRenderer.cs
+++ Project/Assets/Scripts/MazeRenderer.cs
@@ -7,14 +7,14 @@ public class MazeRenderer : MonoBehaviour
 
     [SerializeField]
     [Range(1,50)]
-    private int width = 10;
+    private int width;
 
     [SerializeField]
     private float size = 1f; //size of the prefab (each cell)
 
     [SerializeField]
     [Range(1,50)]
-    private int height = 10;
+    private int height;
 
     [SerializeField]
     private Transform wallPrefab = null;
@@ -33,27 +33,27 @@ public class MazeRenderer : MonoBehaviour
     { 
 
         var floor = Instantiate(floorPrefab, transform);
-        floor.localScale = new Vector3(width, 1, height);
-
+        floor.localScale = new Vector3(size, 1, size);
+        floor.position = new Vector3(-0.5f, 0, -0.5f);
         for(int i=0; i< width; ++i) {
             for (int j=0; j < height; ++j) {
                 var cell = maze[i,j];
-                var position = new Vector3(-width/2 + i, 0, -height/2 + j); //Position of each cell would be offset
+                var position = new Vector3(-width/2 + i, 0.5f, -height/2 + j); //Position of each cell would be offset
 
                 //check if each of the walls exist
                 if(cell.HasFlag(WallState.UP))
                 {
                     var topWall = Instantiate(wallPrefab, transform) as Transform;
-                    topWall.position = position + new Vector3(0, 0, size/2); //Offset
-                    topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                    topWall.position = position + new Vector3(0, 0, 1f/2); //Offset
+                    topWall.localScale = new Vector3(1f, topWall.localScale.y, topWall.localScale.z);
                 }
 
                 //check if cell has a left wall
                 if(cell.HasFlag(WallState.LEFT))
                 {
                     var leftWall = Instantiate(wallPrefab, transform) as Transform;
-                    leftWall.position = position + new Vector3(-size/2, 0, 0);
-                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.position = position + new Vector3(-1f/2, 0, 0);
+                    leftWall.localScale = new Vector3(1f, leftWall.localScale.y, leftWall.localScale.z);
                     leftWall.eulerAngles = new Vector3(0, 90, 0);
                 }
 
@@ -62,8 +62,8 @@ public class MazeRenderer : MonoBehaviour
                     if(cell.HasFlag(WallState.RIGHT))
                     {
                         var rightWall = Instantiate(wallPrefab, transform) as Transform;
-                        rightWall.position = position + new Vector3(+size/2, 0, 0);
-                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                        rightWall.position = position + new Vector3(+1f/2, 0, 0);
+                        rightWall.localScale = new Vector3(1f, rightWall.localScale.y, rightWall.localScale.z);
                         rightWall.eulerAngles = new Vector3(0, 90, 0);
                     }
                 }
@@ -73,8 +73,8 @@ public class MazeRenderer : MonoBehaviour
                     if(cell.HasFlag(WallState.DOWN))
                     {
                         var bottomWall = Instantiate(wallPrefab, transform) as Transform;
-                        bottomWall.position = position + new Vector3(0, 0, -size/2);
-                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                        bottomWall.position = position + new Vector3(0, 0, -1f/2);
+                        bottomWall.localScale = new Vector3(1f, bottomWall.localScale.y, bottomWall.localScale.z);
                     }
                 }
             }
